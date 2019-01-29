cdefeb802da74ee2cc3bc48a263a4812637f3848
Lazurle <arles1219@yahoo.co.jp>
2019-01-30 06:08:41 +0900

    リポジトリの作成、第一部３章のコレクションクラスのHashLIst

---
+++ part1/3,collection/HashList1.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashList1 {
+  public static void main(String args[]){
+    Set<String> colors = new HashSet<String>();//HashSetは重複を許さない
+    colors.add("赤");
+    colors.add("青");
+    colors.add("黄");
+    System.out.println("色の種類は" + colors.size());
+  }
+}
